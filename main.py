2df078d7fae1100106b481777aff59fda669d13b
Erkan Celen <erkanncelen@gmail.com>
2022-11-21 21:29:26 +0100

    add dot in the end of context quote

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils import get_random_quote, generator, insert_record, retreive_posts, render_html
 
 quote = get_random_quote()
-context = quote.partition('.')[0]
+context = quote.partition('.')[0] + "."
 output = generator(context, max_length=300, do_sample=True, temperature=0.9)
 content = output[0]['generated_text'].rsplit('.', 1)[0]+"."
 print(context+'\n'+'\n'+content)
